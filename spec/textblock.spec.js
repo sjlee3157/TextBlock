7c4212696a82aa2c4676e2c987da6422cdfb9446
Sammi-Jo Lee <sjlee3157@gmail.com>
2018-11-14 21:37:40 -0800

    savepoint

--- spec/textblock.spec.js
+++ spec/textblock.spec.js
@@ -19,28 +19,28 @@ describe('TextBlock class',  () =>  {
     });
 
     test('works with a pangram with only lower case', () =>  {
-        const textBlock = new TextBlock('the quick brown fox jumps over the lazy dog');
+        textBlock.setup('the quick brown fox jumps over the lazy dog');
         expect(textBlock.isPangram()).toBe(true);
     });
 
     test("missing character 'x'",  () =>  {
-      const textBlock = new TextBlock('the quick brown fo jumps over the lazy dog');
+      textBlock.setup('the quick brown fo jumps over the lazy dog');
       expect(textBlock.isPangram()).toBe(false);
     });
 
     test('pangram with underscores instead of spaces works', function () {
-      const textBlock = new TextBlock('the_quick_brown_fox_jumps_over_the_lazy_dog');
+      textBlock.setup('the_quick_brown_fox_jumps_over_the_lazy_dog');
       expect(textBlock.isPangram()).toBe(true);
     });
 
     test('pangram with numbers',  () => {
-      const textBlock = new TextBlock('the 1735435quick brown fox jumps over the 98797000laz009y dog');
+      textBlock.setup('the 1735435quick brown fox jumps over the 98797000laz009y dog');
       expect(textBlock.isPangram()).toBe(true);
     });
 
     // Write your own test case
     test('is case-insensitive',  () => {
-      const textBlock = new TextBlock(' uthe 1735435QUICK brown fox jumps oveR rrthe 98797000laz009y dog ');
+      textBlock.setup(' uthe 1735435QUICK brown fox jumps oveR rrthe 98797000laz009y dog ');
       expect(textBlock.isPangram()).toBe(true);
     });
 
--- src/textblock.js
+++ src/textblock.js
@@ -2,13 +2,24 @@
 
 const textBlock = {
   setup(text) {
-    this.text = text;
+    this.text = text.toLocaleLowerCase();
   },
   isPangram() {
-
+// assume it's a string
+// assume no non number/ws/letters
+    for (let character in text) {
+      console.log(text[character])
+    }
   }
 
 
 }
 
 module.exports = textBlock;
+
+
+// const test = new TextBlock('not a pangram');
+// const pangram = text.isPangram();
+
+// const textBlock = new TextBlock('the_quick_brown_fox_jumps_over_the_lazy_dog');
+// expect(textBlock.isPangram()).toBe(true);
